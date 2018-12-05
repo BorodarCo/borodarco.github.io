c923d67d8a60f5e6861f7a0ac7f6c50a07518948
Ivan Gadzhega <ivgadzhega@gmail.com>
2018-12-05 10:30:49 +0200

    Test page for GVAX PoC

---
+++ test/precache-manifest.9452617731227f3c2e8d9958d82a72d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1406c46579b311283fb8",
+    "url": "/static/js/main.1406c465.chunk.js"
+  },
+  {
+    "revision": "9428cc00297e3904f962",
+    "url": "/static/js/1.9428cc00.chunk.js"
+  },
+  {
+    "revision": "1406c46579b311283fb8",
+    "url": "/static/css/main.c4e5db3c.chunk.css"
+  },
+  {
+    "revision": "d39a75f724476a8dccb502f14949f8a4",
+    "url": "/index.html"
+  }
+];
